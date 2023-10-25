707206aace7de960fef4bf95437b34d8b851e0fc
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-10-25 09:21:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak25._10._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite dva broja odvojena razmakom prvi neka bude cijeli broj a drugi decimalan");
+            string s1 = Console.ReadLine();
+            string[] rijeci = s1.Split(' ');
+            int cijelibroj = Convert.ToInt32(rijeci[0]);
+            float decimalan = Convert.ToSingle(rijeci[1]);  
+            Console.WriteLine(cijelibroj * 2);
+            Console.WriteLine(decimalan * 2);
+            Console.ReadKey();
+        }
+    }
+}
